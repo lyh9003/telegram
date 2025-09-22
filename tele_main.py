6385a059d77875305957e36c7b9e9324831ad353
lyh9003 <lyh9003@gmail.com>
2025-09-22 20:34:51 +0900

    Update tele_main.py

--- tele_main.py
+++ tele_main.py
@@ -3,7 +3,7 @@ from telethon.sync import TelegramClient
 
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
-channel = '@sunstudy11'
+channel = 'sunstudy11'
 
 with TelegramClient('my_session', api_id, api_hash) as client:
     for message in client.iter_messages(channel, limit=5):
